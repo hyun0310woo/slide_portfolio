32b6639706777fedb8f5199c8b067b4104549d2b
김현우 <hyun0310woo@gmail.com>
2020-10-30 18:47:35 +0900

    make main slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,43 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../Styles/Slider.css";
 
 const Slider = () => {
   // "count"라는 새로운 상태 값을 정의합니다.
 
+  const [curIdx, count] = useState(0);
+
   return (
     <div id="container">
-      <div class="slide_wrap">
-        <div class="slide_box">
-          <div class="slide_list clearfix">
-            <div class="slide_content slide01">
-              <p>1</p>
+      <div className="slide_wrap">
+        <div className="slide_box">
+          <div className="slide_list clearfix">
+            <div className="slide_content slide01">
+              <p>{curIdx}</p>
             </div>
-            {/* <div class="slide_content slide02">
+            <div className="slide_content slide02">
               <p>2</p>
             </div>
-            <div class="slide_content slide03">
+            <div className="slide_content slide03">
               <p>3</p>
             </div>
-            <div class="slide_content slide04">
+            <div className="slide_content slide04">
               <p>4</p>
             </div>
-            <div class="slide_content slide05">
+            <div className="slide_content slide05">
               <p>5</p>
-            </div> */}
+            </div>
           </div>
           {/* <!-- // .slide_list --> */}
         </div>
         {/* <!-- // .slide_box --> */}
-        <div class="slide_btn_box">
-          <button type="button" class="slide_btn_prev">
+        <div className="slide_btn_box">
+          <button
+            onClick={() => count(curIdx + 1)}
+            type="button"
+            className="slide_btn_prev"
+          >
             Prev
           </button>
-          <button type="button" class="slide_btn_next">
+          <button
+            onClick={() => count(curIdx - 1)}
+            type="button"
+            className="slide_btn_next"
+          >
             Next
           </button>
         </div>
         {/* <!-- // .slide_btn_box --> */}
-        <ul class="slide_pagination"></ul>
+        <ul className="slide_pagination"></ul>
         {/* <!-- // .slide_pagination --> */}
       </div>
       {/* <!-- // .slide_wrap --> */}
