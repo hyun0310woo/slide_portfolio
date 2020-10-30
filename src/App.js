8f52739fe578f30f7986a32227d866b98e5bfb5d
김현우 <hyun0310woo@gmail.com>
2020-10-31 05:46:01 +0900

    make slide_form

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
-// import logo from "./logo.svg";
 import React from "react";
-import Slider from "./components/Slider";
 import "./App.css";
+import Slider from "./Slider";
+
 
 function App() {
   return (
-    <div className="container">
-      <header>
-        <h1>제목</h1>
-      </header>
-      <section className="content">
-        <nav>
-          <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-          </ul>
-        </nav>
-        <main>
-          <Slider />
-        </main>
-        <aside>contactBt</aside>
-      </section>
-      <footer>hyun0310woo@gmail.com</footer>
+    <div className="App">
+      <Slider />
     </div>
-    // <Slider />
   );
 }
 
---
+++ src/Slider.js
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+import "./slider.scss"
+
+function Slider() {
+    let sliderArr = [1,2,3,4,5];
+
+    const [x, setX] = useState(0)
+
+    const goLeft = () => {
+        x === 0 ? setX( -100 * (sliderArr.length - 1)) : setX(x + 100);
+    }
+
+    const goRight = () => {
+       (x === -100 * (sliderArr.length - 1)) ? setX(0) : setX(x - 100);
+    }
+
+    return(
+        <div className="slider">
+            {sliderArr.map((item, index) => {
+                return(
+                    <div key={index} className="slide" style={{transform: `translateX(${x}%)`}}>
+                        {item}
+                    </div>
+                )
+            })}
+            <button id="goLeft" onClick={goLeft}>left</button>
+            <button id="goRight" onClick={goRight}>right</button>
+        </div>
+    )
+}
+
+export default Slider;
