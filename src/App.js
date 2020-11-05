d40349040e6858a0c0b8094b6f677fb2412a7127
김현우 <hyun0310woo@gmail.com>
2020-11-05 21:18:17 +0900

    Fix Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import "./App.css";
+import Navbar from './components/pages/Navbar.';
 
 
 function App() {
   return (
-    <div className="App">
-       <h1>asd</h1>
-    </div>
+    <Router>
+       <Navbar />
+       <Switch>
+         <Route path="/" />
+       </Switch>
+    </Router>
   );
 }
 
--- src/components/pages/Navbar..js
+++ src/components/pages/Navbar..js
@@ -1,19 +1,18 @@
 import React from 'react'
 import { Link } from "react-rouer-dom"
-import { MdFingerprint } from "react-incons/md"
+import { MdFingerprint } from "react-icons/md"
 import { FaBars, FaTimes } from "react-icons/fa"
 
 function Navbar() {
     return (
         <>
           <div className="navbar">
-            <div className="navbar-container container">
+            <div className="navbar-container container container">
                 <Link to="/" className="navbar-logo">
-                    <MdFingerprint className="navbar-incon" />
+                    <MdFingerprint className="navbar-icon" />
                     LAVISH
                 </Link>
                 <div className="menu-icon">
-                                        
                 </div>
             </div>
           </div>
