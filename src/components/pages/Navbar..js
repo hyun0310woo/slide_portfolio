514b4b44e617d6c55f1c5662f4ef1f5c2a19fb4b
김현우 <hyun0310woo@gmail.com>
2020-11-06 23:39:18 +0900

    Fix Navbar

--- src/components/pages/Navbar..js
+++ src/components/pages/Navbar..js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from "react-rouer-dom"
+import { Link } from "react-router-dom"
 import { MdFingerprint } from "react-icons/md"
 import { FaBars, FaTimes } from "react-icons/fa"
 
